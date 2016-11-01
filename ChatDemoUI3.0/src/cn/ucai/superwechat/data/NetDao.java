25e71aea6dac5ed7ee2ff336cb38ac70748dd9e4
HoleWithoutBottom <980507344@qq.com>
2016-11-01 16:43:08 +0800

    添加netDao和Md5文件

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/data/NetDao.java
@@ -0,0 +1,69 @@
+package cn.ucai.superwechat.data;
+
+import android.content.Context;
+
+import java.io.File;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.bean.Result;
+import cn.ucai.superwechat.utils.MD5;
+
+
+/**
+ * 项目名称：WelfareCenter
+ * 创建人：wyl
+ * 时间：2016/10/17 15:18
+ */
+public class NetDao {
+
+    public static void register(Context context, String name, String nick, String password, OkHttpUtils.OnCompleteListener<Result> listener) {
+        OkHttpUtils<Result> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME, name)
+                .addParam(I.User.NICK, nick)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .targetClass(Result.class)
+                .post()
+                .execute(listener);
+    }
+
+    public static void doLogin(Context context, String name, String password, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME, name)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void doFindUserByName(Context context, String name, OkHttpUtils.OnCompleteListener<Result> listener) {
+        OkHttpUtils<Result> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_USER)
+                .addParam(I.User.USER_NAME, name)
+                .targetClass(Result.class)
+                .execute(listener);
+    }
+
+    public static void updateUserAvatar(Context context, String name, File file, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID, name)
+                .addParam(I.AVATAR_TYPE, I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .targetClass(String.class)
+                .post()
+                .execute(listener);
+    }
+
+    public static void updateUserNick(Context context, String name, String nick, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
+                .addParam(I.User.USER_NAME, name)
+                .addParam(I.User.NICK, nick)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+
+
+}
